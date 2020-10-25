07bfd6d3798720caf8d4bb05c16794a614ff6d85
ali <morewater1234@mail.ru>
2020-10-25 11:40:16 +0600

    Second change

--- lesson1/idealweight.rb
+++ lesson1/idealweight.rb
@@ -5,9 +5,9 @@ puts "What's your height? "
 height = Integer(gets.chomp)
 
 ideal_weight = (height - 110) * 1.15
-print "#{name}, your ideal weight is #{ideal_weight}"
+puts '#{name}, your ideal weight is #{ideal_weight}'
 
 if ideal_weight < 0
-    print "#{name}, your weight is ideal now!"
+  puts '#{name}, your weight is ideal now!'
 end
 
