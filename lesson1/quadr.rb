22e8594b8044dee2afa93bc90db3b1d4f4055f05
ali <morewater1234@mail.ru>
2020-10-25 11:40:25 +0600

    Second change

--- lesson1/quadr.rb
+++ lesson1/quadr.rb
@@ -1,26 +1,26 @@
-puts "Enter the a "
-a = gets.chomp.to_i
-print "a = #{a} "
+puts 'Enter the a '
+a = gets.to_i
+puts 'a = #{a} '
 
-puts "\nEnter the b "
-b = gets.chomp.to_i
-print "b = #{b} "
+puts 'Enter the b '
+b = gets.to_i
+puts 'b = #{b} '
 
-puts "\nEnter the c "
-c = gets.chomp.to_i
-print "c = #{c} "
+puts 'Enter the c '
+c = gets.to_i
+puts 'c = #{c} '
 
 d = (b**2) - 4 * a * c
-print "\nD = #{Math.sqrt(d)} "
+puts 'D = #{Math.sqrt(d)} '
 
 if d < 0 
-    print "\nNo rules "
+  puts 'No rules '
 elsif d == 0
-    root = (-b) / 2 * a
-    print "D = #{d} and Root = #{root}"
+  root = (-b) / 2 * a
+  puts 'D = #{d} and Root = #{root}'
 else d > 0
-    disc = Math.sqrt(d)
-    root1 = (-b + disc) / 2 * a
-    root2 = (-b - disc) / 2 * a
-    print "D = #{d}, Root 1 = #{root1}, Root 2 = #{root2}"
+  disc = Math.sqrt(d)
+  root1 = (-b + disc) / 2 * a
+  root2 = (-b - disc) / 2 * a
+  puts 'D = #{d}, Root 1 = #{root1}, Root 2 = #{root2}'
 end
