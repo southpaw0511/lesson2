bb911debb2a7e9ba6c43b429139ddbe11d274ba2
ali <morewater1234@mail.ru>
2020-10-25 22:59:38 +0600

    Third change

--- lesson1/righttriangle.rb
+++ lesson1/righttriangle.rb
@@ -1,22 +1,19 @@
 puts 'Enter the first side of triangle '
 a = gets.to_i
-puts 'A = #{a} '
+puts "A = #{a} "
 
 puts 'Enter the second side of triangle '
 b = gets.to_i
-puts 'B = #{b} '
+puts "B = #{b} "
 
 puts 'Enter the thirth side of triangle '
 c = gets.to_i
-puts 'C = #{c} '
+puts "C = #{c} "
 
-if a > b && a > c 
-  rect = a**2 == (b**2) + (c**2)
-elsif b > a && b > c 
-  rect = b**2 == (a**2) + (c**2)
-else
-  rect = c**2 == (a**2) + (b**2)
-end
+a, b, hypo = [a, b, c].sort
+
+rect = hypo**2 == (a**2) + (b**2)
+puts rect
 
 if rect
 	puts 'Triangle is rectangular '
@@ -28,3 +25,4 @@ if a == b && b == c
   puts "It's isosceles triangle"
 else
   puts "It's not isosceles triangle"
+end
